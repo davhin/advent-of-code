af38d64f6a6a53f83e1930d5e97bd1c17a158fec
David Hinrichs <dvd.hnrchs@gmail.com>
2025-06-05 15:04:38 +0200

    begin rust scaffolding

--- aoc-rs/src/main.rs
+++ aoc-rs/src/main.rs
@@ -1,3 +1,13 @@
+use dotenv::dotenv;
+use std::env;
+
+fn get_filepaths_in_dir(dir_path: String) -> std::io::Result<Vec<String>> {
+    let mut filepaths = Vec::new();
+    Ok(filepaths)
+}
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+    let dir_path = env::var("DATA_DIR").expect("No DATA_DIR is set!");
+    println!("Using data directory {}", dir_path);
 }
